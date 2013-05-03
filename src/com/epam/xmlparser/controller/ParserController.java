a2887b5b2188f4b39a7937fba29ad69ac601fad5
Yury Yaroshevich <Yury_Yaroshevich@epam.com>
2013-05-03 13:35:20 +0300

    initial commit ;

---
+++ src/com/epam/xmlparser/controller/ParserController.java
@@ -0,0 +1,11 @@
+package com.epam.xmlparser.controller;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ParserController extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+		
+	}
+}
